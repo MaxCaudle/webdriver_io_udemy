ea9bd3c11c1ca502003db0ff80a21f8e235392eb
MasonCaiby <maxwell.caudle@gmail.com>
2020-01-03 20:18:39 -0700

    updated the v5

--- tests/contactUsTest.js
+++ tests/contactUsTest.js
@@ -5,20 +5,31 @@ beforeEach(async() => {
 });
 
 describe('Test Contact Us form WebdriverUni', () => {
-	this.timeout(20000);
 
 	it.skip('Should be able to submit a successful submission via contact us form', () => {ContactUs_Page.setFirstName('Joe');
 		ContactUs_Page.submitAllInforamtionViaContactUsForm('Joe', 'Biden','test@test.com', 'Lorem Ipsum');
 		ContactUs_Page.successfulSubmissionSelector.waitForDisplayed(3000);
-		expect(ContactUs_Page.successfulSubmissionSelectorText).to.equal('Thank You for your Message!')
+
+		try {
+			expect(ContactUs_Page.successfulSubmissionSelectorText).to.equal('Thank You for your Message!');
+		} catch(err) {
+			console.log("Exception: " + err);
+			assert.fail();
+		}
+		
 	});
 
-	it.skip('Should not be able to submit a successful submission via contact us form as all fields are required', () =>{
+	it.skip('Should not be able to submit a successful submission via contact us form as all fields are required', () => {
 		ContactUs_Page.setFirstName('Joe');
 		ContactUs_Page.setComments("Lorem ipsum");
 		ContactUs_Page.setEmail("test@test.com");
 		ContactUs_Page.clickSubmitButton();
+		try {
 		expect(ContactUs_Page.unsuccessfulSubmissionSelectorText).to.have('Error: a fields are required');
+	} catch(err) {
+		console.log("Exception: " + err);
+		assert.fail();
+	}
 	});
 
 
